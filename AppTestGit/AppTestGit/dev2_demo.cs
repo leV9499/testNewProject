02e3c655bb4fd860577eb00f7c5bfdcff902943c
Tai Le <letai1854@gmail.com>
2024-03-09 16:02:52 +0700

    create dev2_demo

---
+++ AppTestGit/AppTestGit/dev2_demo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppTestGit
+{
+    public class dev2_demo
+    {
+        public void HelloPeterHelloMarry()
+        {
+            Console.WriteLine("hehehehe");
+        }
+    }
+}
