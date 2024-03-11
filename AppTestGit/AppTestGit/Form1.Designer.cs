cbe35e2910891c4bf3ae07cda58685ce9be3178d
Ho Anh Minh <yamchahuyvs@gmail.com>
2024-03-11 09:13:43 +0700

    [add btn]

--- AppTestGit/AppTestGit/Form1.Designer.cs
+++ AppTestGit/AppTestGit/Form1.Designer.cs
@@ -36,6 +36,7 @@ namespace AppTestGit
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -58,11 +59,22 @@ namespace AppTestGit
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(164, 165);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -76,6 +88,7 @@ namespace AppTestGit
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- AppTestGit/AppTestGit/Form1.cs
+++ AppTestGit/AppTestGit/Form1.cs
@@ -40,5 +40,10 @@ namespace AppTestGit
         {
 
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
